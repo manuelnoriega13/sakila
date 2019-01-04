b22910f9a2c990ffb9415fe7ec13b9966069d9f0
manuel <manuelnoriega13@gmail.com>
2019-01-04 00:48:03 -0300

    pojo actor

--- src/main/java/com/manoriega/sakila/entities/Actor.java
+++ src/main/java/com/manoriega/sakila/entities/Actor.java
@@ -3,6 +3,7 @@ package com.manoriega.sakila.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "actor")
@@ -10,4 +11,15 @@ public class Actor {
 
     @Column(name = "actor_id")
     private Short id;
+
+    @Column(name = "fist_name")
+    private String fistName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "last_update")
+    private Date lastUpdate;
+    
+
 }
